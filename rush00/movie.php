815be3373d7cbf1dfdeda2624b8b6bf07c2049de
Marti FIGUERAS MARTINEZ <mfiguera@e2r9p8.42.fr>
2018-06-03 18:39:02 +0200

    a

--- rush00/movie.php
+++ rush00/movie.php
@@ -11,8 +11,8 @@ if (!$product)
 	exit();
 }
 
-$movie = (array) json_decode(file_get_contents('http://api.themoviedb.org/3/movie/'.$product['databaseid'].'?api_key=9516bcc3f6518e2454476a43925c4625'));
-$credits = (array) json_decode(file_get_contents('http://api.themoviedb.org/3/movie/'.$product['databaseid'].'/credits?api_key=9516bcc3f6518e2454476a43925c4625'));
+$movie = (array) json_decode(file_get_contents('http://api.themoviedb.org/3/movie/'.$product['databaseid'].'?api_key=db663b344723dd2d6781aed1e2f7764d'));
+$credits = (array) json_decode(file_get_contents('http://api.themoviedb.org/3/movie/'.$product['databaseid'].'/credits?api_key=db663b344723dd2d6781aed1e2f7764d'));
 ?>
 
 <div class="movie_container">
@@ -90,12 +90,10 @@ $credits = (array) json_decode(file_get_contents('http://api.themoviedb.org/3/mo
             }
         ?>
     <h3>Crew</h3>
-    <div class="movie_collection">
-
         <?php
             if (isset($credits['crew'])) {
 				$count = 0;
-				echo "<div class='flex_container'>";
+				echo "<div class='movie_collection'>";
                 foreach ($credits['crew'] as $v) {
                     $v = (array)$v;
 					$count++;
@@ -104,12 +102,10 @@ $credits = (array) json_decode(file_get_contents('http://api.themoviedb.org/3/mo
                     if (!empty($v['profile_path']))
 					{
 					?>
-                    <div class="actor">
+                    <div class="movie_box">
                    		<div class="title">
-						<img src=<?php echo 'http://image.tmdb.org/t/p/w185/'.$v['profile_path'];?> alt="image">
-						<p class="name"><?php echo $v['name'];?></p>
-                    	<p>work as:</p>
-						<p class="role"><?php echo $v['job'];?></p>
+						<img class="movie" src=<?php echo 'http://image.tmdb.org/t/p/w185/'.$v['profile_path'];?> alt="image">
+						<div class="title"><?php echo $v['name'];?> as <?php echo $v['job'];?></div>
                     	</div>
                     </div>
 				<?php
