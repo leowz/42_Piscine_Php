15fbba7cf3a32c348c919ad19e7fd296342dc906
Marti FIGUERAS MARTINEZ <mfiguera@e2r9p8.42.fr>
2018-06-03 17:14:06 +0200

    frontend for browse

--- rush00/browse.php
+++ rush00/browse.php
@@ -6,7 +6,7 @@ include_once('partial/header.php');
 
 $categories = category_get_all();
 $movie = product_get_filter($_GET['cat'], (float)$_GET['min'], (float)$_GET['max'], $_GET['name']);
-error_log($movie);
+?error_log($movie);
 ?>
 	<?php if (!empty($_GET))
 	{
@@ -19,18 +19,19 @@ error_log($movie);
 	<div class="filter_section">
 		<h2>Filter</h2>
 		<form action="">
-		<input type="text" name="name" value="<?php echo isset($_GET['name']) ? $_GET['name']: "" ?>" placeholder="name">
-		<input type="number" name="min" value="<?php echo isset($_GET['min']) ? $_GET['min'] : "";?>" placeholder="min price">
-		<input type="number" name="max" value="<?php echo isset($_GET['max']) ? $_GET['max'] : "";?>" placeholder="max price">
+		<input type="text" name="name" value="<?php echo isset($_GET['name']) ? $_GET['name']: "" ?>" placeholder="Title">
+		<input type="number" name="min" value="<?php echo isset($_GET['min']) ? $_GET['min'] : "";?>" placeholder="Minimum price">
+		<input type="number" name="max" value="<?php echo isset($_GET['max']) ? $_GET['max'] : "";?>" placeholder="Maximum prince">
 		<input type="hidden" name="cat" value="<?php echo $_GET['cat']?>">
 		<button type="submit">Search</button>
 		</form>
 		<h2>Category</h2>
-		<ul>
+		<ul class="category">
+			<a class="categoryelem" href='browse.php'><li class="categoryelem">All</li></a>
 			<?php
 			foreach($categories as $c)
 			{
-				echo "<a href='browse.php?cat=" . $c['id'] ."'><li";
+				echo "<a class=\"categoryelem\" href='browse.php?cat=" . $c['id'] ."'><li class=\"categoryelem\"";
 				echo ">" . $c['name'] . "</li></a>";
 			}
 			?>		
@@ -41,14 +42,13 @@ error_log($movie);
 			foreach ($movie as $v)
 			{
 			?>
-			<div class="movie_box">
-				<a href="movie.php?id=<?php echo $v['id']; ?>">
-				  <img src="http://image.tmdb.org/t/p/w185/<?php echo $v['picture']; ?>">
-				  <div class="price"><?php echo number_format($v['price'], 2); ?> ‎€</div>
+			<a href="movie.php?id=<?php echo $v['id']; ?>"><div class="movie_box">
+				
+				  <img class="movie" src="http://image.tmdb.org/t/p/w185/<?php echo $v['picture']; ?>">
 				  <div class="title"><?php echo $v['name']; ?></div>
-				  </a>
-			</div>
-		<?php
+				  <div class="price"><?php echo number_format($v['price'], 2); ?> ‎€</div>
+			</div></a>
+			<?php
 			}
 			?>
 		</div>
